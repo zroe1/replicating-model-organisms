5cbea14f2c1de39fe30fdbcaa730b52f9d3b3af0
zroe1 <zroe@uchicago.edu>
2025-12-20 19:32:41 +0000

    change local vars (breaks local setup)

--- em_organism_dir/global_variables.py
+++ em_organism_dir/global_variables.py
@@ -3,7 +3,7 @@ AZURE_DEPLOYMENT = "gpt-4o"
 AZURE_API_VERSION = "2024-12-01-preview"
 
 # Repository root directory
-BASE_DIR = "/workspace/model-organisms-for-EM"
+BASE_DIR = "/workspace/replicating-model-organisms"
 
 SEED = 0
 
